d3eeff91ecb27c56644a8c3889c64de8b32cb80f
Jonathan Henderson <jkhenderson999@gmail.com>
2021-02-08 21:16:47 -0600

    Add router rework

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig, Route } from 'vue-router';
-import store from '../store';
+import { beforeGuard, afterGuard } from './guards';
 
 // Route-level code-splitting using lazy-loaded components
 // Using components as functions that import the actual component code when called
@@ -45,43 +45,8 @@ const router = new VueRouter({
   routes,
 });
 
-// Function to check if a user has permission to view a specific route
-async function userHasPermission(to: Route) {
-  // Fetch user data if we don't already have it
-  if (!store.state.userFound) {
-    await store.dispatch('verifyUser');
-  }
+router.beforeEach(beforeGuard);
 
-  if (to.name === 'NotFound') {
-    store.commit('updateError', 'Page not found.');
-  }
-
-  return true;
-}
-
-// Async function called before every route load
-router.beforeEach(async (to, _from, next) => {
-  // Set page as currently loading
-  store.commit('updateLoading', true);
-
-  // Check if the user has permission to view their intended route
-  const hasPermission = await userHasPermission(to);
-
-  if (!hasPermission) {
-    store.commit(
-      'updateError',
-      'You do not have permission to view this page!',
-    );
-    next({ name: 'Error' });
-  } else {
-    next();
-  }
-});
-
-// Function called after each route load
-router.afterEach(() => {
-  // Set page as no long loading
-  store.commit('updateLoading', false);
-});
+router.afterEach(afterGuard);
 
 export default router;
